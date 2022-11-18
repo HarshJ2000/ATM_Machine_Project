76d75b1342f9f5a71d35e87620c1bee95f71dae0
Harsh <harsh976324@gmail.com>
2022-11-18 12:29:47 +0530

    Template for Atm options

--- src/com/Harsh/Main.java
+++ src/com/Harsh/Main.java
@@ -14,10 +14,32 @@ public class Main {
         System.out.print("Enter Pin: ");
         int pin = sc.nextInt();
         if ((atmnumber == atmNumber) && (atmpin == pin)) {
-            System.out.println("Validation Done Successfully...");
+            while(true){
+                System.out.println("1. View Available Balance\n2. Withdraw Amount\n3. Deposit Amount\n4. View Mini-Statement\n5. Exit");
+                System.out.println("Enter Choice: ");
+                int ch = sc.nextInt();
+                if (ch == 1) {
+
+                }
+                else if (ch == 2) {
+
+                }
+                else if (ch == 3) {
+
+                }
+                else if (ch == 4) {
+
+                }
+                else if (ch == 5) {
+                    System.out.println("Please Collect Your ATM Card...\n Thank you for using  ATM Machine!!!");
+                    System.exit(0);
+                }else{
+                    System.out.println("Please Enter Correct Choice");
+                }
+            }
         }
         else{
-            System.out.println("Incorrect Atm Number or Pin!!!");
+            System.out.println("Incorrect Atm Number or Pin");
             System.exit(0);
         }
     }
