f1fc2d37fb62b12553b1fbd1703088f26c91a0f7
Harsh <harsh976324@gmail.com>
2022-11-18 12:17:43 +0530

    Base condition for program

--- src/com/Harsh/Main.java
+++ src/com/Harsh/Main.java
@@ -1,8 +1,24 @@
 package com.Harsh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int atmnumber = 12345;
+        int atmpin = 123;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ATM Number: ");
+        int atmNumber = sc.nextInt();
+        System.out.print("Enter Pin: ");
+        int pin = sc.nextInt();
+        if ((atmnumber == atmNumber) && (atmpin == pin)) {
+            System.out.println("Validation Done Successfully...");
+        }
+        else{
+            System.out.println("Incorrect Atm Number or Pin!!!");
+            System.exit(0);
+        }
     }
 }
