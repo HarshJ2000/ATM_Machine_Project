ff74db58e124f88e647bdc1fd5c910e187f82792
Harsh <harsh976324@gmail.com>
2022-11-19 11:26:05 +0530

    Mini-statement option added

--- src/com/Harsh/AtmOperationImpl.java
+++ src/com/Harsh/AtmOperationImpl.java
@@ -1,7 +1,11 @@
 package com.Harsh;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AtmOperationImpl implements AtmOperationInterf{
     ATM atm = new ATM();
+    Map<Double,String> miniStatement = new HashMap<>();
     @Override
     public void viewBalance() {
         System.out.println("Available balance is: " + atm.getBalance());
@@ -9,17 +13,23 @@ public class AtmOperationImpl implements AtmOperationInterf{
 
     @Override
     public void withdrawAmount(double withdrawAmount) {
-        if (atm.getBalance() >= withdrawAmount) {
-            System.out.println("Collect The Cash "+withdrawAmount);
-            atm.setBalance(atm.getBalance()-withdrawAmount);
-            viewBalance();
+        if (withdrawAmount%500 == 0) {
+            if (atm.getBalance() >= withdrawAmount) {
+                miniStatement.put(withdrawAmount," Amount Withdrawn ");
+                System.out.println("Collect The Cash "+withdrawAmount);
+                atm.setBalance(atm.getBalance()-withdrawAmount);
+                viewBalance();
+            }else{
+                System.out.println("Insufficient Balance!!!");
+            }
         }else{
-            System.out.println("Insufficient Balance!!!");
+            System.out.println("Please Enter the Amount in multiples of 500");
         }
     }
 
     @Override
     public void depositAmount(double depositAmount) {
+        miniStatement.put(depositAmount, " Amount Deposited ");
         System.out.println(depositAmount+"Deposited Successfully !!");
         atm.setBalance(atm.getBalance()+depositAmount);
         viewBalance();
@@ -27,6 +37,9 @@ public class AtmOperationImpl implements AtmOperationInterf{
 
     @Override
     public void viewMiniStatement() {
-
+        for (Map.Entry<Double,String> m:miniStatement.entrySet()) {
+            System.out.println(m.getKey()+""+m.getValue());
+        }
+        System.out.println("Total Balance : "+ atm.getBalance()+"\n");
     }
 }
